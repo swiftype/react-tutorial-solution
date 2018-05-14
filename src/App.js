3b577a032632ff78e92c682435c9aacbf7d74518
Jason Stoltzfus <jastoltz24@gmail.com>
2018-05-14 12:08:59 -0400

    Step 5

--- src/App.js
+++ src/App.js
@@ -10,22 +10,34 @@ const client = SwiftypeAppSearch.createClient({
 
 class App extends Component {
   state = {
-    // A new state property, which holds the most recent query response
+    /* A new state property, which tracks value from the search
+     box */
+    queryString: "",
     response: null
   };
 
   componentDidMount() {
-    /* Calling this in componentDidMount ensures that results are
-     displayed on the screen when the app first loads */
-    this.performQuery("node");
+    // Remove hard-coded search for "node"
+    this.performQuery(this.state.queryString);
   }
 
-  // Method to perform a query and store the response
+  /* Handles the `onChange` event every time the user types in
+   the search box. */
+  updateQuery = e => {
+    const queryString = e.target.value;
+    this.setState(
+      {
+        queryString // Save the user entered query string
+      },
+      () => {
+        this.performQuery(queryString); // Trigger a new search
+      }
+    );
+  };
+
   performQuery = queryString => {
     client.search(queryString, {}).then(
       response => {
-        // Add this for now so you can inspect the full response
-        console.log(response);
         this.setState({
           response
         });
@@ -37,7 +49,7 @@ class App extends Component {
   };
 
   render() {
-    const { response } = this.state;
+    const { response, queryString } = this.state;
     if (!response) return null;
 
     return (
@@ -45,14 +57,15 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Node Module Search</h1>
         </header>
-        {/* Query details, like totals and paging data can be found
-         in `response.info`. Here, we are simply showing the total
-         number of results matched by this query. */}
+        {/* A search box, connected to our query string value and onChange handler */}
+        <input
+          className="App-search-box"
+          type="text"
+          placeholder="Enter a search term here"
+          value={queryString}
+          onChange={this.updateQuery}
+        />
         <h2>{response.info.meta.page.total_results} Results</h2>
-        {/* `response.results` contains our list of search results.
-         Each result is wrapped in a `ResultItem` object which has
-         a `getRaw` getter which can be used to access raw field
-         values from the response. */}
         {response.results.map(result => (
           <div key={result.getRaw("id")}>
             <p>Name: {result.getRaw("name")}</p>

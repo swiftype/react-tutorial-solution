56978523cc72bb24c82f4de72930aaee7c2b8aac
Jason Stoltzfus <jastoltz24@gmail.com>
2018-05-14 11:46:34 -0400

    Step 3

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// Import the API client
+import * as SwiftypeAppSearch from "swiftype-app-search-javascript";
+import "./App.css";
+
+// Instantiate a new instance of the Swiftype App Search Javascript Client
+const client = SwiftypeAppSearch.createClient({
+  accountHostKey: process.env.REACT_APP_HOST_KEY, // Configured in .env
+  apiKey: process.env.REACT_APP_SEARCH_KEY, // Configured in .env
+  engineName: "node-modules"
+});
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          {/* Remove boilerplate code and add a new title to the page */}
+          <h1 className="App-title">Node Module Search</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
